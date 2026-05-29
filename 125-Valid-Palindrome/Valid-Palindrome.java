d6334123670afbdfc9668b135e97aada44953600
SaksheeSoni <s.saksheesoni@gmail.com>
2026-05-29 13:00:49 +0530

    Update Valid-Palindrome.java

--- 125-Valid-Palindrome/Valid-Palindrome.java
+++ 125-Valid-Palindrome/Valid-Palindrome.java
@@ -1,1 +1,17 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String cleanedS = s.toLowerCase().replaceAll("[^a-z0-9]", "");
 
+        int left = 0;
+        int right = cleanedS.length()-1;
+
+        while(left < right){
+            if(cleanedS.charAt(left) != cleanedS.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
